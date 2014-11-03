bd99cd38fb4195d8e800c884a363ba2236176b20
pekaras86 <pekaras86@gmail.com>
2014-11-03 17:53:01 +0200

    qqq

--- sign_up_form.php
+++ sign_up_form.php
@@ -59,7 +59,8 @@
 
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <div class="row centered-form">
+        
+		<div class="row centered-form">
             <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -98,8 +99,10 @@
                 </div>
             </div>
         </div>
-      </div>
 
+
+                        
+      </div> <!-- /jumbotron -->
     </div> <!-- /container -->
 
 

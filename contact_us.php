205f0e6b76e144373386b25cc409dd606bda86b2
micprogr <micprogr@users.noreply.github.com>
2014-11-16 14:46:16 +0200

    contact us

--- contact_us.php
+++ contact_us.php
@@ -18,11 +18,15 @@
 			<h2> Contact us </h2>
 			<div class="contact-form">
 				<form action=".." method="POST" enctype="multipart/form-data">
-					<label for="name">Your name:</label><br>
-					<input class="c-input" name="name" type="text" value="" size="30" /><br>
-					<label for="email">Your email:</label><br>
+					<label for="fname">first name: <span class="c-required" style="color:red;">*</span></label><br>
+					<input class="c-input" name="fname" type="text" value="" size="30" /><br>
+					<label for="lname">last name: <span class="c-required"  style="color:red;">*</span></label><br>
+					<input class="c-input" name="lname" type="text" value="" size="30" /><br>
+					<label for="email">email address: <span class="c-required"  style="color:red;">*</span></label><br>
 					<input class="c-input" name="email" type="text" value="" size="30" /><br>
-					<label for="message">Your message:</label><br>
+					<label for="phone">phone number:</label><br>
+					<input class="c-input" name="phone" type="text" value="" size="30" /><br>
+					<label for="message">your message:</label><br>
 					<textarea class="c-input" name="message" rows="10" cols="40"></textarea><br>
 					<input type="submit" value="Send email">
 				</form>

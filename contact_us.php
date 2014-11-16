d726af8998741c63332a46a19d5ab3f18c5a9a40
pekaras86 <pekaras86@gmail.com>
2014-11-16 21:28:13 +0200

    update contuct us

--- contact_us.php
+++ contact_us.php
@@ -16,19 +16,29 @@
 	<div class="container">
 		<div class="jumbotron">
 			<h2> Contact us </h2>
-			<div class="contact-form">
-				<form action=".." method="POST" enctype="multipart/form-data">
+			<div class="contact-form" style="width:40%;">
+				<form action=".." method="POST" enctype="multipart/form-data" role="form">
+				  <div class="form-group col-sm-12 col-lg-12">
 					<label for="fname">first name: <span class="c-required" style="color:red;">*</span></label><br>
-					<input class="c-input" name="fname" type="text" value="" size="30" /><br>
+					<input class="c-input form-control" name="fname" type="text" value="" size="30" /><br>
+				  </div>	
+				  <div class="form-group col-lg-12">	
 					<label for="lname">last name: <span class="c-required"  style="color:red;">*</span></label><br>
-					<input class="c-input" name="lname" type="text" value="" size="30" /><br>
+					<input class="c-input form-control" name="lname" type="text" value="" size="30" /><br>
+				  </div>	
+				  <div class="form-group col-lg-12">	
 					<label for="email">email address: <span class="c-required"  style="color:red;">*</span></label><br>
-					<input class="c-input" name="email" type="text" value="" size="30" /><br>
+					<input class="c-input form-control" name="email" type="text" value="" size="30" /><br>
+				  </div>
+                  <div class="form-group col-lg-12">				  
 					<label for="phone">phone number:</label><br>
-					<input class="c-input" name="phone" type="text" value="" size="30" /><br>
+					<input class="c-input form-control" name="phone" type="text" value="" size="30" /><br>
+				  </div>
+				  <div class="form-group">
 					<label for="message">your message:</label><br>
-					<textarea class="c-input" name="message" rows="10" cols="40"></textarea><br>
-					<input type="submit" value="Send email">
+					<textarea class="c-input form-control" name="message" rows="10" cols="40"></textarea><br>
+					<input type="submit" value="Send email" class="btn btn-default">
+				  </div>
 				</form>
 			</div>
 		</div>

ad54d61fcf4b21d524724479ff376432f07daec3
pekaras86 <pekaras86@gmail.com>
2014-11-20 19:56:05 +0200

    about_us apdate

--- about_us.php
+++ about_us.php
@@ -17,12 +17,19 @@
     <div class="container"> 
 	  
 	 <div class="jumbotron">
+      <div class="row"> 
+	   <p style="font-size:15px; text-align:center;">We are a group of local Greek Car Rental Companies. All of our members are  registered with the Greek Tourism Organization and comply with <br>
+	   current regulations. All our members have been operating successfully in the Greek market , either under their own logo or as franchisees of major <br>
+	   international firms, for many years now and have joined forces to reach new clientele through internet.</p> 
        
-	  
-        
+       <p style="font-size:15px; text-align:center;">We simply want to give you a clean, safe, reliable car coupled with a personal touch and a friendly smile.</p>	   
 		
-      
-      </div> <!-- / jumbotron-->
+	   <p style="font-size:15px; text-align:center;">We hope to be able to provide you with our service during your stay in Greece making your stay unforgetable!</p> 
+        <div style="display:table; margin: 0 auto;">
+	     <img src="images/char-icons/keychain.png" class="img-responsive" alt="Responsive image" style="width:200px;">
+        </div>	   
+      </div>
+     </div> <!-- / jumbotron-->
 
     </div> <!-- /container -->
 

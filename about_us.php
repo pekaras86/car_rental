5731a360325aefa678fdc52806f694063c019fc6
micprogr <micprogr@gmail.com>
2014-11-22 18:03:01 +0200

    adjusted about as

--- about_us.php
+++ about_us.php
@@ -18,13 +18,11 @@
 	  
 	 <div class="jumbotron">
       <div class="row"> 
-	   <p style="font-size:15px; text-align:center;">We are a group of local Greek Car Rental Companies. All of our members are  registered with the Greek Tourism Organization and comply with <br>
-	   current regulations. All our members have been operating successfully in the Greek market , either under their own logo or as franchisees of major <br>
-	   international firms, for many years now and have joined forces to reach new clientele through internet.</p> 
-       
-       <p style="font-size:15px; text-align:center;">We simply want to give you a clean, safe, reliable car coupled with a personal touch and a friendly smile.</p>	   
-		
-	   <p style="font-size:15px; text-align:center;">We hope to be able to provide you with our service during your stay in Greece making your stay unforgetable!</p> 
+           <p style="font-size:15px;">Dear Clients,</p>
+	   <p style="font-size:15px;">Welcome to Thessaloniki Car Rentals, a highly specialized car rental company based in Thessaloniki, Greece.</p>
+           <p style="font-size:15px;">We offer you a wide variety of brand new and safe vehicles at the best prices in the market.<br>You can find Thessaloniki Car Rentals branches at the biggest cities throughout Greece,<br> including Athens, Thessaloniki, Chania, Larissa, Alexandroupoli and more. </p>	
+	   <p style="font-size:15px;">Our main priority is your complete satisfaction,<br>so we provide you with the highest quality service possible, making your stay unforgetable!</p>
+           <p style="font-size:15px;">The Thessaloniki Car Rentals Team</p> 
         <div style="display:table; margin: 0 auto;">
 	     <img src="images/char-icons/keychain.png" class="img-responsive" alt="Responsive image" style="width:200px;">
         </div>	   

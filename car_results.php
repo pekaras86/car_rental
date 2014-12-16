f47d8f1727cc3500dcb1458e44db16fdb50d36a2
micprogr <micprogr@gmail.com>
2014-12-16 16:53:17 +0200

    car results update

--- car_results.php
+++ car_results.php
@@ -23,9 +23,10 @@
     <!-- Main component for a primary marketing message or call to action -->
     <div class="container"> 
 	  
-	 <div class="jumbotron">
+<!--	 <div class="jumbotron"> -->
 
-		<div class="row">
+		<div class="row no-side-margins">
+
 	  <?php  
 	 	  
 	  // pianei ta stoixeia apo ti forma	   
@@ -34,10 +35,63 @@
 	  setcookie('dropoff_location', $_REQUEST['dropoff_location']);
 	  setcookie('pickup_date', $_REQUEST['pickup_date']);
 	  setcookie('dropoff_date', $_REQUEST['dropoff_date']);
-	  
-	  
-	  
-	  // dimiourgia query anazitisis sti vasi
+	 
+	  echo <<<EOD
+	  	<div class="calendar-module-2 col-xs-12 col-s-12 col-md-5 col-lg-5"">  
+		<form role="form" action="car_results.php" method="POST">
+		   <div class="form-group pickdrop_lock"> <!--start pickup location-->
+		     <label>Pickup Location</label>
+             <select class="form-control" id="pickup_location" name="pickup_location">
+               <option name="pickup_location" value="ATHENS AIRPORT">ATHENS AIRPORT</option>
+               <option name="pickup_location" value="THESSALONIKI AIRPORT">THESSALONIKI AIRPORT</option>
+               <option name="pickup_location" value="VOLOS">VOLOS</option>
+               <option name="pickup_location" value="ALEKSANDROUPOLI">ALEKSANDROUPOLI</option>
+			   <option name="pickup_location" value="CHANIA">CHANIA</option>
+             </select>
+		    </div> <!--end pickup location-->
+		  <div class="form-group pickdrop_date">  <!--start pickup date-->
+		    <label>Pickup Date</label>
+            <input type="text" class="form-control date" id="datepicker1" name="pickup_date">
+		  </div> <!--end pickup date-->
+		  <div class="form-group pickdrop_time">  <!--start pickup time--> 
+            <label>Pickup Time</label>		  
+			<input type="text" class="form-control" id="timepicker1">
+		  </div> <!--end pickup time-->
+		  <div class="form-group pickdrop_lock"> <!--start drop-off location-->
+		     <label>Drop Off Location</label>
+             <select class="form-control" id="dropoff_location" name="dropoff_location">
+               <option name="dropoff_location" value="ATHENS AIRPORT">ATHENS AIRPORT</option>
+               <option name="dropoff_location" value="THESSALONIKI AIRPORT">THESSALONIKI AIRPORT</option>
+               <option name="dropoff_location" value="VOLOS">VOLOS</option>
+               <option name="dropoff_location" value="ALEKSANDROUPOLI">ALEKSANDROUPOLI</option>
+			   <option name="dropoff_location" value="CHANIA">CHANIA</option>
+             </select>
+		    </div> <!--end dropoff location-->
+			<div class="form-group pickdrop_date">  <!--start dropoff date-->
+		    <label>Drop Off Date</label>
+            <input type="text" class="form-control date" id="datepicker2" name="dropoff_date">
+		  </div> <!--end dropoff date-->
+		  <div class="form-group pickdrop_time">  <!--start dropoff time--> 
+            <label>Drop Off Time</label>		  
+			<input type="text" class="form-control" id="timepicker2">
+		  </div> <!--end dropoff time-->
+		  <div class="form-group car-category"> <!--start drop-off location-->
+		     <label>Category</label>
+             <select class="form-control" id="car-category" name="category">
+               <option name="category" value="Any">ANY</option>
+               <option name="category" value="City Car">CITY CAR</option>
+               <option name="category" value="Station Wagon">STATION WAGON</option>
+               <option name="category" value="Suv">SUV</option>
+             </select>
+		    </div> <!--end dropoff location-->
+		  <div class="form-group search_mod_button">        
+            <button type="submit" class="btn btn-default btn-info search-module">SEARCH</button>
+          </div>
+		</form>
+	  </div>
+EOD;
+
+     // dimiourgia query anazitisis sti vasi
 	  if ($category == 'Any') {
 	    $select_query = "SELECT cars.id, cars.name, car_categories.name as car_category, cars.description, car_locations.name as car_location, price, pic_path FROM cars " . 
 						"INNER JOIN car_locations ON car_locations.id = cars.location_ID " .
@@ -50,7 +104,8 @@
 					  "WHERE active = 1 AND car_categories.name = '{$category}' " .
 					  "AND car_locations.name = '{$pickup_location}'";
 	    }
-	  // run the query	
+
+	   // run the query	
 	  $result = mysqli_query($con, $select_query);  
     	  
 	  //emfanise ola ta amaksia
@@ -165,68 +220,15 @@ EOD;
 		} //end while
 	  
 	  }	 else {
-			echo  '<h4 id="reults_count" style="color:red">No cars found. Please repeat your search.</h4>';
+			echo '<h4 id="reults_count" style="color:red">No cars found. Please repeat your search.</h4>';
+			echo '<div class="car_result_restricted"> </div>';
 	  }
 	?>
-
-
-<div class="calendar-module-2 col-xs-12 col-s-12 col-md-5 col-lg-5">  
-		<form role="form" action="car_results.php" method="POST">
-		   <div class="form-group pickdrop_lock"> <!--start pickup location-->
-		     <label>Pickup Location</label>
-             <select class="form-control" id="pickup_location" name="pickup_location">
-               <option name="pickup_location" value="ATHENS AIRPORT">ATHENS AIRPORT</option>
-               <option name="pickup_location" value="THESSALONIKI AIRPORT">THESSALONIKI AIRPORT</option>
-               <option name="pickup_location" value="VOLOS">VOLOS</option>
-               <option name="pickup_location" value="ALEKSANDROUPOLI">ALEKSANDROUPOLI</option>
-			   <option name="pickup_location" value="CHANIA">CHANIA</option>
-             </select>
-		    </div> <!--end pickup location-->
-		  <div class="form-group pickdrop_date">  <!--start pickup date-->
-		    <label>Pickup Date</label>
-            <input type="text" class="form-control date" id="datepicker1" name="pickup_date">
-		  </div> <!--end pickup date-->
-		  <div class="form-group pickdrop_time">  <!--start pickup time--> 
-            <label>Pickup Time</label>		  
-			<input type="text" class="form-control" id="timepicker1">
-		  </div> <!--end pickup time-->
-		  <div class="form-group pickdrop_lock"> <!--start drop-off location-->
-		     <label>Drop Off Location</label>
-             <select class="form-control" id="dropoff_location" name="dropoff_location">
-               <option name="dropoff_location" value="ATHENS AIRPORT">ATHENS AIRPORT</option>
-               <option name="dropoff_location" value="THESSALONIKI AIRPORT">THESSALONIKI AIRPORT</option>
-               <option name="dropoff_location" value="VOLOS">VOLOS</option>
-               <option name="dropoff_location" value="ALEKSANDROUPOLI">ALEKSANDROUPOLI</option>
-			   <option name="dropoff_location" value="CHANIA">CHANIA</option>
-             </select>
-		    </div> <!--end dropoff location-->
-			<div class="form-group pickdrop_date">  <!--start dropoff date-->
-		    <label>Drop Off Date</label>
-            <input type="text" class="form-control date" id="datepicker2" name="dropoff_date">
-		  </div> <!--end dropoff date-->
-		  <div class="form-group pickdrop_time">  <!--start dropoff time--> 
-            <label>Drop Off Time</label>		  
-			<input type="text" class="form-control" id="timepicker2">
-		  </div> <!--end dropoff time-->
-		  <div class="form-group car-category"> <!--start drop-off location-->
-		     <label>Category</label>
-             <select class="form-control" id="car-category" name="category">
-               <option name="category" value="Any">ANY</option>
-               <option name="category" value="City Car">CITY CAR</option>
-               <option name="category" value="Station Wagon">STATION WAGON</option>
-               <option name="category" value="Suv">SUV</option>
-             </select>
-		    </div> <!--end dropoff location-->
-		  <div class="form-group search_mod_button">        
-            <button type="submit" class="btn btn-default btn-info search-module">SEARCH</button>
-          </div>
-		</form>
-	  </div>
-				</div>
+	</div>  <!-- / row -->
         
 	    
 		
-	 </div> <!-- / jumbotron-->
+	<!-- </div> <!-- / jumbotron-->
 
     </div> <!-- /container -->
 

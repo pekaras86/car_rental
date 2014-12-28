4dbeccac2d92626ebe2fd5a5cdd699d1ef487b12
George Papadopoulos <gpapadop79@yahoo.gr>
2014-12-28 14:40:20 +0200

    set charset to utf8
    gia na apothikeuei swsta ta ellinika

--- scripts/database_connection.php
+++ scripts/database_connection.php
@@ -3,6 +3,9 @@
 require_once ('app_config.php'); 
 
 $con = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME);
+
+mysqli_set_charset($con,'utf8'); //set character set gia na apothikeuei swsta ellinikous xaraktires
+
 if(mysqli_connect_errno($con)){
 	die("Failed to connect to mySQL!" . mysqli_error($con) . "<br>");
 } 
